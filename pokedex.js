791545c5a2f8e2e38608264fa58c775a31830d7f
Claarainiesta7 <iniesta.llop@bootcamp-upgrade.com>
2022-03-11 16:33:46 +0100

    files

---
+++ pokedex.js
@@ -0,0 +1,39 @@
+//const pokemonURL = "https://pokeapi.co/api/v2/pokemon/"; //almaceno la URL en la api
+const todosLosPokemons = 151; //El nº que necesito recoger de pokemons
+
+const gallery$$ = document.querySelector(".gallery"); //creo el nodo con mi div con clase "gallery"
+
+function getCards(){
+for (let index = 1; index < todosLosPokemons; index++) {
+    buscarPokemon(index)
+}}
+
+function buscarPokemon(index){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
+    .then((res) => res.json())
+    .then((resParsedToJSON) => {
+        console.log(resParsedToJSON);
+        printCards(resParsedToJSON);
+    })
+}
+
+
+const printCards = (card) => {
+    console.log("card", card);
+        const figure$$ = document.createElement("figure");
+        const title$$ = document.createElement("h2");
+        const image$$ = document.createElement("img");
+        const type$$ = document.createElement("p");
+
+        title$$.textContent = card.name;
+        image$$.src = card.sprites.other['official-artwork'].front_default;
+        type$$.textContent = card.types.map ((item) => item.type.name);
+
+        figure$$.appendChild(title$$);
+        figure$$.appendChild(image$$);
+        gallery$$.appendChild(figure$$);
+        figure$$.appendChild(type$$);
+
+}
+
+getCards();
